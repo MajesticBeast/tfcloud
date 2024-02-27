e35dc92fafa62fcfa9716f3d02321119208321dc
MajesticBeast <majestic@thelostsons.net>
2024-02-26 18:08:49 -0800

    update

---
+++ main.tf
@@ -0,0 +1,30 @@
+resource "random_pet" "bucket_suffix" {
+  length = 5
+  separator = "-"
+}
+
+module "tags" {
+  source = "app.terraform.io/thelostsons/tags/aws"
+  version = "0.0.3"
+
+    owner = "mybucket${random_pet.bucket_suffix.id}"
+    environment = "dev"
+    project = "myproject"
+}
+
+module "s3" {
+  source = "app.terraform.io/thelostsons/s3/aws"
+  version = "0.0.4"
+
+  bucket = "${var.bucket}${random_pet.bucket_suffix.id}"
+
+  tags = module.tags.tags
+}
+
+module "iam" {
+  source = "app.terraform.io/thelostsons/iam/aws"
+  version = "0.0.1"
+
+  name = "timmay"
+  tags = module.tags.tags
+}
